e9bdc57c580142b6c26d8d47ae0cb0e86038d176
DKhorobrykh <horobryh.daniil@yandex.ru>
2025-03-18 11:53:45 +0300

    :white_check_mark: add simple test

---
+++ src/test/java/unit/MainTest.java
@@ -0,0 +1,14 @@
+package unit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        assertTrue(true);
+    }
+
+}
