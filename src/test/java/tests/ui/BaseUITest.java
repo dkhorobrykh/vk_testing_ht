e7ebfc07dc5a96ff4a33e0cc7234fdfc00452e71
DKhorobrykh <horobryh.daniil@yandex.ru>
2025-03-27 05:39:27 +0300

    :bug: trying to fix pipeline

--- src/test/java/tests/ui/BaseUITest.java
+++ src/test/java/tests/ui/BaseUITest.java
@@ -2,9 +2,23 @@ package tests.ui;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseUITest {
+
+    // for github actions
+    @BeforeAll
+    public static void setUp() {
+        String userDataDir = System.getenv("CHROME_USER_DATA_DIR");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--user-data-dir=" + userDataDir);
+
+        Configuration.browserCapabilities = options;
+    }
 
     @AfterEach
     public void tearDown() {
